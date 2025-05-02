a70e42449041ea73658bf134d296bba3be8ba0f2
Xsc <59879111@qq.com>
2025-05-02 18:25:07 +0800

    Update xb-custom-nav.php

--- xb-custom-nav.php
+++ xb-custom-nav.php
@@ -147,7 +147,7 @@ function xb_nav_display_page() {
     ?>
     <div class="xb-nav-container">
         <?php if ($show_page_title === '1'): ?>
-            <h1 class="entry-title"><?php echo esc_html($page_title); ?></h1>
+            <div class="xb-title"><?php echo esc_html($page_title); ?></div>
         <?php endif; ?>
 
         <!-- 搜索板块 -->
